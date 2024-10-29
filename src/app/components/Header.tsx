cf42c3bbd746b749ba29ef5239bf94a6e7668564
MahanoorHabib <mahanoormahanoor15@gmail.com>
2024-10-30 03:26:38 +0500

    first commit

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Header(){
+    return(
+        <div className='flex gap-6 justify-between px-4 bg-blue-600'>
+            <div>LOGO</div>
+            <div className='cursur-pointer'>Home</div>
+            <div className='cursur-pointer'>About Us</div>
+            <div className='cursur-pointer'>Services</div>
+            <div className='cursur-pointer'>Contact Us</div>
+            
+            </div> 
+                    
+                
+            
+
+        
+        
+    )
+}
